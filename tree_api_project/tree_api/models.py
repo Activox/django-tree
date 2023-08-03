1f45047bfb8acefc7d5f023c2d7d4ddf20329f19
Activox <PAULOTTENWALDER@ALUMNO.UTESA.EDU>
2023-08-02 20:48:30 -0400

    creating the models and serializers of the project

--- tree_api_project/tree_api/models.py
+++ tree_api_project/tree_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Tree(models.Model):
+    value = models.CharField(max_length=30)
+    deleted = models.BooleanField(default=False)
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children"
+    )
+
+    def __str__(self):
+        return self.value
